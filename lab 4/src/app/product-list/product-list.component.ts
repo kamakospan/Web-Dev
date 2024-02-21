61f962f0626ee2e6e6c0b2083a0ab0696814f7fe
kamakospan <kamazhayokama@gmail.com>
2024-02-21 20:54:11 +0600

    lab4

---
+++ lab 4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share(link: string) {
+    const shareMessage = `Product:  ${link}`;
+    const telegramLink = `https://t.me/share/url?url=${encodeURIComponent(
+      shareMessage
+    )}`;
+    window.location.href = telegramLink;
+  }
+
+  share_kaspi(link: string) {
+    window.location.href = link;
+  }
+}
