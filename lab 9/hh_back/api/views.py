420b860cb5f34b89d11d2867aa393e59336a478b
kamakospan <kamazhayokama@gmail.com>
2024-04-10 00:04:52 +0500

    lab 9 finished successfully

---
+++ lab 9/hh_back/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
+# Create your views here.
+
+class CompanyListAPIView(generics.ListAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class CompanyDetailAPIView(generics.RetrieveAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    lookup_field = 'id'
+
+class CompanyVacanciesAPIView(generics.ListAPIView):
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):
+        company_id = self.kwargs['id']
+        return Vacancy.objects.filter(company_id = company_id)
+    
+class VacancyListAPIView(generics.ListAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+class VacancyDetailAPIView(generics.RetrieveAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    lookup_field = 'id'
+
+class TopTenVacanciesAPIView(generics.ListAPIView):
+    queryset = Vacancy.objects.order_by('salary')[:10]
+    serializer_class = VacancySerializer
